752f682cc5ea129a4656a0bb9455b455e611d6eb
Gerardo Matadama Peralta <gerardo.matadama@gmail.com>
2024-05-19 23:00:00 -0600

    delete debug tag

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       child: BlocBuilder<ThemeBloc, ThemeState>(
         builder: (context, state) {
           return MaterialApp(
+            debugShowCheckedModeBanner: false,
             title: 'Flutter Demo',
             theme: state.themeData,
             home: Homescreen()
